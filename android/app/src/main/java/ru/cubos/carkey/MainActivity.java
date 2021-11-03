8640ba70da26f69be51fd8dccf0975196b8c0783
Vitaly <yacubovvs@yandex.ru>
2021-11-04 02:25:26 +0300

    Created android part project

---
+++ android/app/src/main/java/ru/cubos/carkey/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.cubos.carkey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("Tiburon e-key");
+    }
+}
